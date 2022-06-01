e8d01606b17233a1d36b4ac462cbe562d27dd868
Michał Matczak <michalmatczak22@gmail.com>
2022-06-01 13:37:50 +0200

    fixed crop

--- data_processing/image_processing.py
+++ data_processing/image_processing.py
@@ -73,6 +73,27 @@ def plot_xywhn_annotated_image_from_df(img_path: str, df_annotation: pd.DataFram
     plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
 
+def plot_xywhn_annotated_array_from_df(img: np.array, df_annotation: pd.DataFrame):
+    """
+    Plot image with bounding box from dataframe.
+
+    :param img: image array
+    :param df_annotation: dataframe with xywhn format annotation
+    """
+    img_copy = img.copy()
+    height, width, _ = img_copy.shape
+
+    for index, row in df_annotation.iterrows():
+        sample_w = float(row['width']) * width
+        sample_h = float(row['height']) * height
+        x1, y1 = float(row['xcenter']) * width - sample_w / 2, float(row['ycenter']) * height - sample_h / 2
+        x2, y2 = x1 + sample_w, y1 + sample_h
+        cv2.rectangle(img_copy, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
+
+    plt.figure(figsize=(12, 8))
+    plt.imshow(img_copy)
+
+
 def get_brightness_stats(source_dir: str) -> pd.DataFrame:
     """
     Calculate avg brightness for every image in given source directory.
@@ -106,24 +127,3 @@ def get_number_of_objects_stats(source_dir: str) -> pd.DataFrame:
         annotations = pd.read_csv(f'{source_dir}/{filename}', dtype=str, sep=' ')
         stats.append([filename, len(annotations.index)])
     return pd.DataFrame(stats, columns=['filename', 'number of objects'])
-
-
-def plot_xywhn_annotated_array_from_df(img: np.array, df_annotation: pd.DataFrame):
-    """
-    Plot image with bounding box from dataframe.
-
-    :param img: image array
-    :param df_annotation: dataframe with xywhn format annotation
-    """
-    img_copy = img.copy()
-    height, width, _ = img_copy.shape
-
-    for index, row in df_annotation.iterrows():
-        sample_w = float(row['width']) * width
-        sample_h = float(row['height']) * height
-        x1, y1 = float(row['xcenter']) * width - sample_w / 2, float(row['ycenter']) * height - sample_h / 2
-        x2, y2 = x1 + sample_w, y1 + sample_h
-        cv2.rectangle(img_copy, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
-
-    plt.figure(figsize=(12, 8))
-    plt.imshow(img_copy)
