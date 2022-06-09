edeabad933f8b01945d35ace828ac2b483e8d87f
Chotom <tomaszczochan@gmail.com>
2022-06-09 17:19:29 +0200

    Added heridal ipsar dataset notebook, refactored notebook for private dataset, adjustments in crop augmentation method

--- README.md
+++ README.md
@@ -10,12 +10,13 @@ datasets.
 
 List of used datasets in the project. Follow instructions in notebooks to prepare data.
 
-| Dataset name              | Data preparation notebook                                                              | Source                                                                  | Resolutions                      |
-|---------------------------|----------------------------------------------------------------------------------------|-------------------------------------------------------------------------|----------------------------------|
-| VisDrone Object Detection | [visdrone_object_detection](data_processing/notebooks/visdrone_object_detection.ipynb) | [Link - aiskyeye.com](http://aiskyeye.com/download/object-detection-2/) | Various (and scales 4:3 or 16:9) |
-| Private Dataset           | [private_dataset](data_processing/notebooks/private_dataset.ipynb)                     | Unavailable at this moment                                              |                                  |
-| RGB Combined Dataset      | [rgb_combined_dataset](data_processing/notebooks/rgb_combined_dataset.ipynb)           | Processed and combined RGB datasets                                     |                                  |
-| RGB Augmented Dataset     | [rgb_augmented_dataset](data_processing/notebooks/rgb_augmented_dataset.ipynb)         | Processed and combined RGB datasets with augmentation                   |                                  |
+| Dataset name                | Data preparation notebook                                                              | Source                                                                           |
+|-----------------------------|----------------------------------------------------------------------------------------|----------------------------------------------------------------------------------|
+| VisDrone Object Detection   | [visdrone_object_detection](data_processing/notebooks/visdrone_object_detection.ipynb) | [Link - aiskyeye.com](http://aiskyeye.com/download/object-detection-2/)          |
+| Heridal IPSAR               | [heridal_ipsar](data_processing/notebooks/heridal_ipsar.ipynb)                         | [Link - ipsar.fesb.unist.hr](http://ipsar.fesb.unist.hr/HERIDAL%20database.html) |
+| Private Dataset             | [private_dataset](data_processing/notebooks/private_dataset.ipynb)                     | Unavailable at this moment                                                       |
+| RGB Combined Dataset        | [rgb_combined_dataset](data_processing/notebooks/rgb_combined_dataset.ipynb)           | Processed and combined RGB datasets                                              |
+| TODO: RGB Augmented Dataset | [rgb_augmented_dataset](data_processing/notebooks/rgb_augmented_dataset.ipynb)         | Processed and combined RGB datasets with augmentation                            |
 
 ## Data augmentation
 
--- data_processing/image_augmentation.py
+++ data_processing/image_augmentation.py
@@ -1,3 +1,4 @@
+import os
 import random
 
 import pandas as pd
@@ -5,24 +6,27 @@ import numpy as np
 import albumentations as A
 import cv2
 
+
 def read_yolo_annotation_to_list(annotations_path: str):
     try:
         bboxes = pd.read_csv(annotations_path,
                              names=['label', 'x', 'y', 'w', 'h'],
                              dtype={'label': str, 'x': float, 'y': float, 'w': float, 'h': float}, sep=' ')
-    except:
+    except FileNotFoundError:
         return []
 
     return bboxes[['x', 'y', 'w', 'h', 'label']].values.tolist()
 
 
 def selected_crop(image_path: str,
-                annotation_path: str,
-                new_width=512,
-                new_height=512,
-                new_filename='',
-                new_image_dir='',
-                new_annotation_dir=''):
+                  annotation_path: str,
+                  new_width=256,
+                  new_height=256,
+                  new_filename='',
+                  new_image_dir='',
+                  new_annotation_dir='',
+                  annotation_count=5,
+                  scale_height=1080):
     """
     For every object in the given image use crop function and save cropped images with annotations.
 
@@ -33,13 +37,23 @@ def selected_crop(image_path: str,
     :param new_filename: Filename suffix to save image and annotation (without extension).
     :param new_image_dir: Directory path to save cropped image.
     :param new_annotation_dir: Directory path to save cropped image annotations in xywhn (YOLO) format.
+    :param annotation_count: Max number of random annotations to crop.
+    :param scale_height: value to scale image = image height / scale_height.
     :return: calculated new image and annotations
     """
+    assert new_image_dir != ''
+    assert new_annotation_dir != ''
+    os.makedirs(new_image_dir, exist_ok=True)
+    os.makedirs(new_annotation_dir, exist_ok=True)
+
     image = cv2.imread(image_path)
     height, width, _ = image.shape
     annotation_list = read_yolo_annotation_to_list(annotation_path)
+    annotation_count = min(annotation_count, len(annotation_list))
+    scale = height / scale_height
+    new_height, new_width = int(new_height * scale), int(new_width * scale)
 
-    for i, annotation in enumerate(annotation_list):
+    for i, annotation in enumerate(random.sample(annotation_list, annotation_count)):
         sample_w = float(annotation[2]) * width
         sample_h = float(annotation[3]) * height
         x1, y1 = float(annotation[0]) * width - sample_w / 2, float(annotation[1]) * height - sample_h / 2
@@ -52,20 +66,26 @@ def selected_crop(image_path: str,
         crop_x2 = crop_x1 + new_width
         crop_y2 = crop_y1 + new_height
 
-        transform = A.Compose([
+        # Images with objects visibility between min and max will not be saved.
+        transform_min_vis = A.Compose([
             A.Crop(x_min=crop_x1, y_min=crop_y1, x_max=crop_x2, y_max=crop_y2)
-        ], bbox_params=A.BboxParams(format='yolo', min_visibility=0.5))
-        transformed = transform(image=image, bboxes=annotation_list)
-        transformed_image = transformed['image']
-        transformed_annotation = transformed['bboxes']
+        ], bbox_params=A.BboxParams(format='yolo', min_visibility=0.2))
+        transformed = transform_min_vis(image=image, bboxes=annotation_list)
+        transformed_min_annotation = transformed['bboxes']
 
-        if new_image_dir != '' and new_annotation_dir != '':
+        transform_max_vis = A.Compose([
+            A.Crop(x_min=crop_x1, y_min=crop_y1, x_max=crop_x2, y_max=crop_y2)
+        ], bbox_params=A.BboxParams(format='yolo', min_visibility=0.8))
+        transformed = transform_max_vis(image=image, bboxes=annotation_list)
+        transformed_max_annotation = transformed['bboxes']
+        transformed_image = transformed['image']
+
+        # Save image
+        if np.shape(transformed_max_annotation) == np.shape(transformed_min_annotation):
             image_filename = f'{new_image_dir}/{new_filename}_{i}.jpg'
             annotation_filename = f'{new_annotation_dir}/{new_filename}_{i}.txt'
             cv2.imwrite(f'{image_filename}', transformed_image)
 
-            df_annotation = pd.DataFrame(transformed_annotation, columns=['x', 'y', 'w', 'h', 'label'])
+            df_annotation = pd.DataFrame(transformed_max_annotation, columns=['x', 'y', 'w', 'h', 'label'])
             df_annotation = df_annotation[['label', 'x', 'y', 'w', 'h']]
             df_annotation.to_csv(annotation_filename, sep=' ', header=False, index=False)
-
-    #return img, new_annotations  # todo zawsze zwróć przerobione zdjęcie
